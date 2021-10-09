12f22c0569caac643b1e09799b6e4c4f0111a3cc
Keita-Ide <73172721+Keita-Ide@users.noreply.github.com>
2021-10-09 17:19:07 +0900

    #4 (#9)
    * Set An Appbar #2
    
    * Create a login screen
    
    * delete theme and remain default style activated
    
    * Change mui-component from item-elevation to Grid
    
    * Create login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router';
-import Header from './features/header';
-import aquarium from './features/aquarium';
-import counterBoard from './containers/example';
-import './App.css';
-
-// App.tsxにAPI通信の制御を一元化　→　次回対応
-// import { useEffect, useState } from 'react';
-// import fetch from 'node-fetch';
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import Header from "./features/header";
+import aquarium from "./features/aquarium";
+import login from "./features/login";
+import counterBoard from "./containers/example";
+import "./App.css";
 
 const App: React.FC = () => {
-
-  // API通信の一元化は次回対応
-  // const [query, setQuery] = useState([]);
-  // const [data, setData] = useState([]);
-
-  // GET用
-  // useEffect(() => {
-  //   // 開発用URL
-  //   const URL = `http://localhost:8080/api/aquariummer/${query}`;
-  //   // 本番用URL
-  //   // const URL = `http://aquariummer/api/aquariummer/${query}`;
-  //   const appInit = {
-  //     method: 'GET',
-  //     mode: 'cors',
-  //     cache: 'default',
-  //   };
-
-  //   // fetch(URL, appInit).then( res => res.json())
-
-  //     fetch(URL, appInit)
-  //     .then(res => 
-  //       res.json()
-  //     )
-  //     .then(json => {
-  //       setQuery(json['query'])
-  //       setData(json['data'])
-  //     })
-  // }, [query])
-
   return (
     <>
       <Header />
       <Switch>
-        <Route exact path="/" component={aquarium} />
+        <Route exact path="/login" component={login} />
         <Route exact path="/example/CounterBoard" component={counterBoard} />
         <Redirect from="/user/profile/:userId" to="/user/:userId" />
         <Route path="/user/:userId" component={aquarium} />
+        <Route exact path="/" component={aquarium} />
         <Redirect push to="/" />
       </Switch>
     </>
-  )
-}
+  );
+};
 
 export default App;
