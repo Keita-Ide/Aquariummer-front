72618facf2da86179553e2d6a13b70d3ca7fba26
Keita-Ide <73172721+Keita-Ide@users.noreply.github.com>
2021-10-09 17:41:53 +0900

    Develop (#13)
    * first commit and create remote branch of develop
    
    * #4 (#9)
    
    * Set An Appbar #2
    
    * Create a login screen
    
    * delete theme and remain default style activated
    
    * Change mui-component from item-elevation to Grid
    
    * Create login screen
    
    * #11 (#12)
    
    * Delete Counte test files and remain slice files
    
    * modify path

--- src/containers/example/CounterBoad.tsx
+++ src/containers/example/CounterBoad.tsx
@@ -1,22 +1,22 @@
-import React, { FC } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // import { add, decrement, increment } from 'actions';
 // import { CounterState } from 'reducer';
-import { CounterState, counterSlice } from '../../features/counter';
-import CounterBoad from '../../components/example/CounterBoad';
+import { CounterState, counterSlice } from "../../features/counter";
+import CounterBoad from "../../components/example/CounterBoad";
 
 const EnhancedCounterBoard: FC = () => {
-    const count = useSelector<CounterState, number>((state) => state.count);
-    const dispatch = useDispatch();
+  const count = useSelector<CounterState, number>((state) => state.count);
+  const dispatch = useDispatch();
 
-    return (
-        <CounterBoad
-            count={count}
-            add={(amount: number) => dispatch(counterSlice.actions.added(amount))}
-            decrement={() => dispatch(counterSlice.actions.decremented())}
-            increment={() => dispatch(counterSlice.actions.incremented())}
-        />
-    );
+  return (
+    <CounterBoad
+      count={count}
+      add={(amount: number) => dispatch(counterSlice.actions.added(amount))}
+      decrement={() => dispatch(counterSlice.actions.decremented())}
+      increment={() => dispatch(counterSlice.actions.incremented())}
+    />
+  );
 };
 
 export default EnhancedCounterBoard;
