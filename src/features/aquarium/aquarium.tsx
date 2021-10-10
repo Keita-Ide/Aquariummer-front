aabceea16d752e2aed7fd148d60615f2a9697614
Keita-Ide <73172721+Keita-Ide@users.noreply.github.com>
2021-10-11 08:12:21 +0900

    Develop into Master (#16)
    * first commit and create remote branch of develop
    
    * #4 (#9)
    
    * Set An Appbar #2
    
    * Create a login screen
    
    * delete theme and remain default style activated
    
    * Change mui-component from item-elevation to Grid
    
    * Create login screen
    
    * #11 (#12)
    
    * Delete Counte test files and remain slice files
    
    * modify path
    
    * Cpmplete tasks which this ISSUE gonna do. (#15)
    
    * Cpmplete tasks which this ISSUE gonna do.
    
    * Add comment and modification
    
    * Finished #14 (#20)
    
    * Capcha screen and modify readme (#21)

--- src/features/aquarium/aquarium.tsx
+++ src/features/aquarium/aquarium.tsx
@@ -10,53 +10,56 @@ import Grid from "@material-ui/core/Grid";
 import Chip from "@mui/material/Chip";
 import samplePicture from "../../images/samplePicture.jpg";
 
-type AquariumValue = {
-  aquariumName: string;
-  aquariumWater: string;
-  aquariumSize: string;
-  aquariumWaterSize: string;
-  fishNamesNumbers: [
-    {
-      fishName: string;
-      fishNumber: number;
-    }
-  ];
-  aquariumEquipment: {
-    heater: string;
-    filter: string;
-    soil: string;
-    sand: string;
-    feeding: string;
-    chemical: string;
-  };
-};
+type AquariumValue = [
+  {
+    aquariumName: string;
+    aquariumWater: string;
+    aquariumSize: string;
+    aquariumWaterSize: string;
+    fishNamesNumbers: [
+      {
+        fishName: string;
+        fishNumber: number;
+      }
+    ];
+    aquariumEquipment: {
+      heater: string;
+      filter: string;
+      soil: string;
+      sand: string;
+      feeding: string;
+      chemical: string;
+    };
+  }
+];
 
-const InitialState: AquariumValue = {
-  aquariumName: "未設定",
-  aquariumWater: "未設定",
-  aquariumSize: "未設定",
-  aquariumWaterSize: "未設定",
-  fishNamesNumbers: [
-    {
-      fishName: "未設定",
-      fishNumber: 0,
+const InitialState: AquariumValue = [
+  {
+    aquariumName: "未設定",
+    aquariumWater: "未設定",
+    aquariumSize: "未設定",
+    aquariumWaterSize: "未設定",
+    fishNamesNumbers: [
+      {
+        fishName: "未設定",
+        fishNumber: 0,
+      },
+    ],
+    aquariumEquipment: {
+      heater: "未設定",
+      filter: "未設定",
+      soil: "未設定",
+      sand: "未設定",
+      feeding: "未設定",
+      chemical: "未設定",
     },
-  ],
-  aquariumEquipment: {
-    heater: "未設定",
-    filter: "未設定",
-    soil: "未設定",
-    sand: "未設定",
-    feeding: "未設定",
-    chemical: "未設定",
   },
-};
+];
 
 const Aquarium: FC = () => {
   const [aquariumData, setAquariumData] = useState<AquariumValue>(InitialState);
   const [alertFlug, setAlertFlug] = useState(false);
 
-  // 不要なレンダリングを避ける
   useEffect(() => {
     const URL = "http://localhost:8080/api/aquariummer/";
     const aquariumInit = {
@@ -82,46 +85,58 @@ const Aquarium: FC = () => {
           }}
           severity="success"
         >
-          TIt's under development! Have fun!
+          It's under development! Have fun!
         </Alert>
       )}
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea
-          onClick={() => {
-            setAlertFlug(true);
-          }}
-        >
-          <CardMedia
-            component="img"
-            height="140"
-            image={samplePicture}
-            alt="aquarium picture"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {aquariumData?.aquariumName}
-            </Typography>
-            <Grid container direction="row" justifyContent="center" spacing={1}>
-              <Grid>
-                <Chip label={aquariumData?.aquariumWater} />
-                <Chip label={aquariumData?.aquariumSize + "cm水槽"} />
-                <Chip label={aquariumData?.aquariumWaterSize + "L"} />
-                <Chip label={aquariumData?.fishNamesNumbers[0]?.fishName} />
-                <Chip
-                  label={aquariumData?.fishNamesNumbers[0]?.fishNumber + "匹"}
-                />
-                <Chip label={aquariumData?.aquariumEquipment?.heater} />
-                <Chip label={aquariumData?.aquariumEquipment?.filter} />
-                <Chip label={aquariumData?.aquariumEquipment?.soil} />
-                <Chip label={aquariumData?.aquariumEquipment?.sand} />
-                <Chip label={aquariumData?.aquariumEquipment?.soil} />
-                <Chip label={aquariumData?.aquariumEquipment?.feeding} />
-                <Chip label={aquariumData?.aquariumEquipment?.chemical} />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <Grid container direction="row" justifyContent="flex-start" spacing={1}>
+        {aquariumData.map((aquariumData, index: number) => (
+          <Card sx={{ maxWidth: 345, m: 1 }} key={index}>
+            <CardActionArea
+              onClick={() => {
+                // Transit to Aquarium-Detail-Screen. It doesn't exist now, so I temporally show aleart.
+                setAlertFlug(true);
+              }}
+            >
+              <CardMedia
+                component="img"
+                height="140"
+                image={samplePicture}
+                alt="aquarium picture"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {aquariumData?.aquariumName}
+                </Typography>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  spacing={1}
+                >
+                  <Grid>
+                    <Chip label={aquariumData?.aquariumWater} />
+                    <Chip label={aquariumData?.aquariumSize + "cm水槽"} />
+                    <Chip label={aquariumData?.aquariumWaterSize + "L"} />
+                    <Chip label={aquariumData?.fishNamesNumbers[0]?.fishName} />
+                    <Chip
+                      label={
+                        aquariumData?.fishNamesNumbers[0]?.fishNumber + "匹"
+                      }
+                    />
+                    <Chip label={aquariumData?.aquariumEquipment?.heater} />
+                    <Chip label={aquariumData?.aquariumEquipment?.filter} />
+                    <Chip label={aquariumData?.aquariumEquipment?.soil} />
+                    <Chip label={aquariumData?.aquariumEquipment?.sand} />
+                    <Chip label={aquariumData?.aquariumEquipment?.soil} />
+                    <Chip label={aquariumData?.aquariumEquipment?.feeding} />
+                    <Chip label={aquariumData?.aquariumEquipment?.chemical} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Grid>
     </>
   );
 };
