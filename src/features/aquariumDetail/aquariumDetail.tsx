3a70515b791d67fe96db2ceea212d6bd57fed6f9
Keita-Ide <Keita-Ide@github.com>
2021-10-14 00:12:46 +0900

    Complete Task

--- src/features/aquariumDetail/aquariumDetail.tsx
+++ src/features/aquariumDetail/aquariumDetail.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import fetch from "node-fetch";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
@@ -6,29 +7,79 @@ import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
 import samplePicture from "../../images/samplePicture.jpg";
 
-interface State {
+type AquariumDetailValue = {
+  aquariumId: string;
   aquariumName: string;
   aquariumWater: string;
   aquariumSize: string;
   aquariumWaterSize: string;
-  fishNamesNumbers: string;
-  aquariumEquipment: string;
-}
+  fishNamesNumbers: [
+    {
+      fishName: string;
+      fishNumber: number;
+    }
+  ];
+  aquariumEquipment: {
+    heater: string;
+    filter: string;
+    soil: string;
+    sand: string;
+    feeding: string;
+    chemical: string;
+  };
+};
+
+const InitialState: AquariumDetailValue = {
+  aquariumId: "未設定",
+  aquariumName: "未設定",
+  aquariumWater: "未設定",
+  aquariumSize: "未設定",
+  aquariumWaterSize: "未設定",
+  fishNamesNumbers: [
+    {
+      fishName: "未設定",
+      fishNumber: 0,
+    },
+  ],
+  aquariumEquipment: {
+    heater: "未設定",
+    filter: "未設定",
+    soil: "未設定",
+    sand: "未設定",
+    feeding: "未設定",
+    chemical: "未設定",
+  },
+};
 
 const aquariumDetail: React.FC = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [values, setValues] = React.useState<State>({
-    aquariumName: "",
-    aquariumWater: "",
-    aquariumSize: "",
-    aquariumWaterSize: "",
-    fishNamesNumbers: "",
-    aquariumEquipment: "",
-  });
+  const [values, setValues] = React.useState<AquariumDetailValue>(InitialState);
   const handleChangeText =
-    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    (prop: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
       setValues({ ...values, [prop]: event.target.value });
     };
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  React.useEffect(() => {
+    const URL = "http://localhost:8080/api/aquariummer/aquarium";
+    const aquariumInit = {
+      method: "POST",
+      mode: "cors",
+      cache: "default",
+      params: {
+        aquariumId: values.aquariumId,
+      },
+    };
+
+    // BE開発後コメントアウト解除
+    // fetch(URL, aquariumInit)
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     // setValues(JSON.parse(JSON.stringify(json)));
+    //     console.log(JSON.parse(JSON.stringify(json)));
+    //   });
+  }, [values]);
+
   return (
     <>
       <Grid
@@ -42,8 +93,8 @@ const aquariumDetail: React.FC = () => {
           <CardMedia
             component="img"
             alt="aquarium"
-            height="500"
-            width="550"
+            height="520"
+            width="500"
             image={samplePicture}
           />
         </Card>
@@ -54,57 +105,135 @@ const aquariumDetail: React.FC = () => {
             justifyContent="flex-start"
             spacing={1}
           >
-            <Grid container item>
-              <Grid item xs={4}>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
                 <InputLabel>水槽名</InputLabel>
                 <TextField
+                  fullWidth
                   required
                   id="aquariumName"
+                  value={values?.aquariumName}
                   onChange={handleChangeText("aquariumName")}
                 />
               </Grid>
-              <Grid item xs={4}>
+              <Grid item xs={5}>
                 <InputLabel>水質</InputLabel>
                 <TextField
+                  fullWidth
                   required
                   id="aquariumWater"
+                  value={values?.aquariumWater}
                   onChange={handleChangeText("aquariumWater")}
                 />
               </Grid>
             </Grid>
-            <Grid container item>
-              <Grid item xs={4}>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
                 <InputLabel>水槽サイズ</InputLabel>
                 <TextField
+                  fullWidth
                   required
                   id="aquariumSize"
+                  value={values?.aquariumSize}
                   onChange={handleChangeText("aquariumSize")}
                 />
               </Grid>
-              <Grid item xs={4}>
+              <Grid item xs={5}>
                 <InputLabel>水槽の容量</InputLabel>
                 <TextField
+                  fullWidth
                   required
                   id="aquariumWaterSize"
+                  value={values?.aquariumWaterSize}
                   onChange={handleChangeText("aquariumWaterSize")}
                 />
               </Grid>
             </Grid>
-            <Grid container item>
-              <Grid item xs={4}>
-                <InputLabel>魚</InputLabel>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
+                <InputLabel>魚種</InputLabel>
                 <TextField
+                  fullWidth
+                  required
+                  id="fishNamesKinds"
+                  value={values?.fishNamesNumbers[0].fishName}
+                  onChange={handleChangeText("fishNamesKinds")}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <InputLabel>魚数</InputLabel>
+                <TextField
+                  fullWidth
                   required
                   id="fishNamesNumbers"
+                  value={values?.fishNamesNumbers[0].fishNumber}
                   onChange={handleChangeText("fishNamesNumbers")}
                 />
               </Grid>
-              <Grid item xs={4}>
-                <InputLabel>設備</InputLabel>
+            </Grid>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
+                <InputLabel>ヒーター</InputLabel>
                 <TextField
+                  fullWidth
                   required
-                  id="aquariumEquipment"
+                  id="heater"
+                  value={values?.aquariumEquipment.heater}
                   onChange={handleChangeText("aquariumEquipment")}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <InputLabel>フィルター</InputLabel>
+                <TextField
+                  fullWidth
+                  required
+                  id="filter"
+                  value={values?.aquariumEquipment.filter}
+                  onChange={handleChangeText("filter")}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
+                <InputLabel>底床剤</InputLabel>
+                <TextField
+                  fullWidth
+                  required
+                  id="soil"
+                  value={values?.aquariumEquipment.soil}
+                  onChange={handleChangeText("soil")}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <InputLabel>底砂</InputLabel>
+                <TextField
+                  fullWidth
+                  required
+                  id="sand"
+                  value={values?.aquariumEquipment.sand}
+                  onChange={handleChangeText("sand")}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item columnGap={3}>
+              <Grid item xs={5}>
+                <InputLabel>飼料</InputLabel>
+                <TextField
+                  fullWidth
+                  required
+                  id="feeding"
+                  value={values?.aquariumEquipment.feeding}
+                  onChange={handleChangeText("feeding")}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <InputLabel>薬品</InputLabel>
+                <TextField
+                  fullWidth
+                  required
+                  id="chemical"
+                  value={values?.aquariumEquipment.chemical}
+                  onChange={handleChangeText("chemical")}
                 />
               </Grid>
             </Grid>
